cb1aface5b878a36598c75656c39e29c67b506fa
Robbie Lankford <rlankford@richcontext.com>
2020-06-09 17:25:26 -0500

    re-implement Parameters Read() function

--- parameter.go
+++ parameter.go
@@ -31,17 +31,29 @@ func NewParameters(basePath string, parameters map[string]*Parameter) *Parameter
 
 //Parameters holds the output and all AWS Parameter Store that have the same base path
 type Parameters struct {
+	i          int64 // read index
+	bytesJSON  []byte
 	basePath   string
 	parameters map[string]*Parameter
 }
 
 //Read implements the io.Reader interface for the key/value pair
 func (p *Parameters) Read(des []byte) (n int, err error) {
-	bytesJSON, err := json.Marshal(p.getKeyValueMap())
-	if err != nil {
-		return 0, err
+	if p.bytesJSON == nil {
+		p.bytesJSON, err = json.Marshal(p.getKeyValueMap())
+		if err != nil {
+			return 0, err
+		}
 	}
-	return copy(des, bytesJSON), io.EOF
+
+	if p.i >= int64(len(p.bytesJSON)) {
+		return 0, io.EOF
+	}
+
+	n = copy(des, p.bytesJSON[p.i:])
+	p.i += int64(n)
+
+	return n, nil
 }
 
 //GetValueByName returns the value based on the name
--- parameter_test.go
+++ parameter_test.go
@@ -1,8 +1,11 @@
 package awsssm
 
 import (
+	"bytes"
+	"math/rand"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type env struct {
@@ -113,9 +116,68 @@ func TestParameters_Decode(t *testing.T) {
 	}
 }
 
+func TestParameters_Read(t *testing.T) {
+	tests := []struct {
+		name              string
+		basePath          string
+		parameters        map[string]*Parameter
+		expectedError     error
+		expectedByteCount int64
+	}{
+		{
+			name:              "ReadFrom Small Byte Count",
+			basePath:          "/my-service/dev",
+			parameters:        getParametersMap(),
+			expectedError:     nil,
+			expectedByteCount: 70,
+		},
+		{
+			name: "ReadFrom Large Byte Count",
+			basePath: "/my-service/dev",
+			parameters: getRandomParametersMap(1000, 10),
+			expectedError: nil,
+			expectedByteCount: 33001,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			parameter := NewParameters(test.basePath, test.parameters)
+			buf := new(bytes.Buffer)
+			bytesRead, err := buf.ReadFrom(parameter)
+			if err != test.expectedError {
+				t.Errorf(`Unexpected error: got %s, expected %s`, err, test.expectedError)
+			}
+			if bytesRead != test.expectedByteCount {
+				t.Errorf(`Unexpected value: got %d, expected %d`, bytesRead, test.expectedByteCount)
+			}
+		})
+	}
+}
+
 func getParametersMap() map[string]*Parameter {
 	return map[string]*Parameter{
 		"/my-service/dev/DB_PASSWORD": {Value: param1.Value},
 		"/my-service/dev/DB_HOST":     {Value: param2.Value},
 	}
+}
+
+var seededRand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func getRandomParametersMap(n int, l int) map[string]*Parameter {
+	m := make(map[string]*Parameter)
+	for i := 0; i < n; i++ {
+		s := "/my-service/dev/"
+		for j := 0; j < l; j++ {
+			char := charset[seededRand.Intn(int(^uint(0)>>1))%len(charset)]
+			s += string(char)
+		}
+		m[s] = &Parameter{
+			Value: param1.Value,
+		}
+	}
+	return m
 }
